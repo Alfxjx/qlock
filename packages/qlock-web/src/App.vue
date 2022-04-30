cc9c9f47cc0dcc341dd4fcc60f8e7697340b59e2
xujx <xjxtju@163.com>
2022-04-30 12:42:22 +0800

    init

--- packages/qlock-web/src/App.vue
+++ packages/qlock-web/src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="wrapper">
+			<QlockTwo></QlockTwo>
+		</div>
+		<footer>
+			QlockTwo © by
+			<a
+				href="https://www.abandon.work"
+				target="_blank"
+				rel="noopener noreferrer"
+				>Alfxjx</a
+			>
+			with
+			<a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+				<span>♥</span>
+			</a>
+		</footer>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QlockTwo from "./components/QlockTwo.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		QlockTwo,
+	},
+};
 </script>
 
 <style>
+body {
+	margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background: #fefefe;
+	font-family: Monaco, sans-serif;
+}
+.wrapper {
+	flex: 1;
+	display: flex;
+	align-items: center;
+}
+footer {
+	flex: 0;
+	padding: 1rem 0;
+	color: rgb(128, 128, 128);
+}
+a {
+	color: #0bc2bf;
+	text-decoration: none;
+}
+span {
+	color: red;
 }
 </style>
